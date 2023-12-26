252c949d83c08453b064727422a69c5cb5f46031
Navyazitya <151536566+Navyazitya@users.noreply.github.com>
2023-12-26 15:18:31 +0530

    Add files via upload
    A small password validation code

---
+++ pass.js
@@ -0,0 +1,62 @@
+var myInput=document.getElementById("pass");
+var letter=document.getElementById("letter");
+var capital=document.getElementById("capital");
+var number=document.getElementById("number");
+var length = document.getElementById("length");
+
+
+myInput.onfocus=function()
+{
+document.getElementById("message").style.display="block";
+}
+
+myInput.onblur=function()
+{
+document.getElementById("message").style.display="none";
+}
+
+myInput.onkeyup=function()
+{
+    var lowercaseletters=/[a-z]/g;
+    if(myInput.value.match(lowercaseletters))
+    {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    }
+    else{
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+    var uppercaseletters=/[A-Z]/g;
+    if(myInput.value.match(uppercaseletters))
+    {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    }
+    else{
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
+
+    var numbers=/[0-9]/g;
+    if(myInput.value.match(numbers))
+    {
+       number.classList.remove("invalid");
+        number.classList.add("valid");
+    }
+    else{
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
+
+    if(myInput.value.length>=8)
+    {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    }
+    else{
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
+}
